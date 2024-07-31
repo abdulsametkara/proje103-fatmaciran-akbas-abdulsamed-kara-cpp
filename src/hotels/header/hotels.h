1548895f62dfa05b6d9ef08dcd077199d0958087
abdulsametkara <abdulsamedkra@gmail.com>
2024-07-31 23:55:45 +0300

    add room, categorize room, update room, delete room functions completed

--- src/hotels/header/hotels.h
+++ src/hotels/header/hotels.h
@@ -20,6 +20,16 @@ typedef struct {
     char password[100];
 } User;
 
+typedef struct {
+    int roomNumber;       // Oda numarasý
+    char roomType[100];   // Oda tipi
+    double price;         // Oda fiyatý
+    int capacity;         // Oda kapasitesi
+    char amenities[200]; // Bu alanda odanýn sunduðu imkanlarý tutacaðýz.
+
+} Room;
+
+
 int mainMenu(istream& in, ostream& out);
 int RoomManagementMenu(istream& in, ostream& out);
 int customerManagentMenu(istream& in, ostream& out);
@@ -27,4 +37,15 @@ int financialsMenu(istream& in, ostream& out);
 int additionalServicesMenu(istream& in, ostream& out);
 int integrationsMenu(istream& in, ostream& out);
 
+
+
+int loadRooms(Room** rooms, const char* pathFileRooms);
+int saveRooms(Room* rooms, int roomCount, const char* pathFileRooms, istream& in, ostream& out);
+int addRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
+int updateRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
+int addRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
+int updateRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
+int deleteRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
+int printRoomsByType(const char* pathFileRooms, const char* roomType, istream& in, ostream& out);
+int categorizeRoomMenu(const char* pathFileRooms, istream& in, ostream& out);
 #endif // HOTELS_H
